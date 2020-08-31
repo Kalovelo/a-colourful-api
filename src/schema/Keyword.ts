5ce9364553c60d17c1e4f697bfe544c14d28f5c3
Kalovelo <apostoloskalovelonis@gmail.com>
2020-09-01 01:06:21 +0300

    feat: keyword mutations

--- src/schema/Keyword.ts
+++ src/schema/Keyword.ts
@@ -1,11 +1,64 @@
 import { GraphQLObjectType, GraphQLString, GraphQLID } from "graphql";
+import Keyword, { KeywordDocument } from "../models/Keyword";
+
+export interface Keyword {
+  id: string;
+  name: string;
+  svg: string;
+}
 
 export const KeywordType = new GraphQLObjectType({
   name: "Keyword",
   fields: () => ({
     id: { type: GraphQLID! },
     name: { type: GraphQLString! },
-    description: { type: GraphQLString! },
     svg: { type: GraphQLString! },
   }),
 });
+
+const addKeyword = {
+  type: KeywordType,
+  args: {
+    name: { type: GraphQLString! },
+    svg: { type: GraphQLString! },
+  },
+  async resolve(parent: Keyword, args: any) {
+    const keyword = new Keyword({
+      name: args.name,
+      svg: args.svg,
+    });
+    return await keyword.save();
+  },
+};
+
+const updateKeyword = {
+  type: KeywordType,
+  args: {
+    name: { type: GraphQLString },
+    svg: { type: GraphQLString },
+  },
+  async resolve(parent: Keyword, args: any) {
+    let data: { name?: string | null; svg?: String } = {};
+    if (args.name) data.name = args.name;
+    if (args.svg) data.svg = args.svg;
+    let keyword = await Keyword.findOneAndUpdate({ _id: args.id }, data as KeywordDocument, {
+      new: true,
+      runValidators: true,
+    });
+    if (!keyword) throw new Error("Keyword ID not found.");
+  },
+};
+
+const deleteKeyword = {
+  type: KeywordType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: Keyword, args: any) {
+    let keyword = await Keyword.findByIdAndDelete(args.id);
+    if (!keyword) throw new Error("Keyword ID not found.");
+    return keyword;
+  },
+};
+
+export const keywordMutations = { addKeyword, updateKeyword, deleteKeyword };
--- src/schema/Root.ts
+++ src/schema/Root.ts
@@ -1,19 +1,27 @@
 import {
   GraphQLObjectType,
-  GraphQLString,
   GraphQLList,
-  GraphQLEnumType,
   GraphQLID,
   GraphQLSchema,
+  GraphQLString,
+  ObjectTypeDefinitionNode,
 } from "graphql";
 import { TopicType } from "./Topic";
 import Topic from "../models/Topic";
 import { KeywordType } from "./Keyword";
-import keyword from "../models/Keyword";
+import Keyword from "../models/Keyword";
+import { keywordMutations } from "./Keyword";
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    topics: {
+      type: new GraphQLList(TopicType),
+      args: { id: { type: GraphQLID } },
+      async resolve(parent, args) {
+        return await Topic.find();
+      },
+    },
     topic: {
       type: TopicType,
       args: { id: { type: GraphQLID } },
@@ -22,17 +30,30 @@ const RootQuery = new GraphQLObjectType({
       },
     },
     keywords: {
+      type: new GraphQLList(KeywordType),
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Keyword.find();
+      },
+    },
+    keyword: {
       type: KeywordType,
       args: { id: { type: GraphQLID } },
       async resolve(parent, args) {
-        return await keyword.find();
+        return await Keyword.findById(args.id);
       },
     },
   },
 });
 
+const RootMutation = new GraphQLObjectType({
+  name: "RootMutation",
+  fields: { ...keywordMutations },
+});
+
 const RootQuerySchema = new GraphQLSchema({
   query: RootQuery,
+  mutation: RootMutation,
 });
 
 export default RootQuerySchema;
